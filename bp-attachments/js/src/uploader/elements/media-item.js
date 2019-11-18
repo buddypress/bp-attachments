99c43293ffbbbaa0e1e55f14fa8a719c86483aa3
imath <math.viet@gmail.com>
2019-11-18 14:21:51 +0100

    Display the details of a media into a Modal

--- bp-attachments/js/src/uploader/elements/media-item.js
+++ bp-attachments/js/src/uploader/elements/media-item.js
@@ -1,9 +1,11 @@
 /**
  * WordPress dependencies
  */
-const { Component, createElement } = wp.element;
+const { Component, createElement, Fragment } = wp.element;
+const { Button, Modal } = wp.components;
 const { compose } = wp.compose;
 const { withDispatch } = wp.data;
+const { sprintf, __ } = wp.i18n;
 
 /**
  * Internal dependencies
@@ -13,7 +15,22 @@ import setTemplate from '../utils/set-template';
 class MediaItem extends Component {
 	constructor() {
 		super( ...arguments );
+
+		this.state = {
+			isOpen: false,
+		};
+
 		this.onMediaClick = this.onMediaClick.bind( this );
+		this.openModal = this.openModal.bind( this );
+		this.closeModal = this.closeModal.bind( this );
+	}
+
+	openModal() {
+		this.setState( { isOpen: true } );
+	}
+
+	closeModal() {
+		this.setState( { isOpen: false } );
 	}
 
 	onMediaClick() {
@@ -22,17 +39,37 @@ class MediaItem extends Component {
 		if ( 'inode/directory' === mimeType ) {
 			return mediaOpen( { name: name } );
 		}
+
+		return this.openModal();
 	}
 
     render() {
 		const Template = setTemplate( 'bp-attachments-media-item' );
+		const { isOpen } = this.state;
+		const { title, vignette } = this.props;
 
         return (
-			<div
-				className="media-item"
-				dangerouslySetInnerHTML={ { __html: Template( this.props ) } }
-				onClick={ this.onMediaClick }
-			/>
+			<Fragment>
+				<div
+					className="media-item"
+					dangerouslySetInnerHTML={ { __html: Template( this.props ) } }
+					onClick={ this.onMediaClick }
+				/>
+				{ isOpen && (
+					<Modal
+						title={ sprintf( __( 'Details for: %s', 'bp-attachments' ), title ) }
+						onRequestClose={ this.closeModal }>
+
+						{ vignette && (
+							<img src={vignette} className="mediaDetails" />
+						) }
+
+						{ ! vignette && (
+							<p>{ __( '@todo Fetch the Media properties.', 'bp-attachments' ) }</p>
+						) }
+					</Modal>
+				) }
+			</Fragment>
         );
     }
 }
