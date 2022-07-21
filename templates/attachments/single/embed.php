8e08debbe23acae4ec06112b1f49d5c2bcc6fca9
imath <math.viet@gmail.com>
2022-07-21 22:56:47 +0200

    Add files forgotten in ddbcefbad7fda825cfd45135db1e6f6e78b3251e

---
+++ templates/attachments/single/embed.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * BP Attachments single embed template.
+ *
+ * @package \bp-attachments\templates\attachments\embed
+ *
+ * @since 1.0.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<p>Nicely embedded!</p>
---
+++ templates/attachments/single/view.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * BP Attachments single view template.
+ *
+ * @package \bp-attachments\templates\attachments\view
+ *
+ * @since 1.0.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<p>Nice view!</p>
