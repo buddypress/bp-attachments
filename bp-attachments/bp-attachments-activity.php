1cf970ab0ca8ea2370570bed95cd662c48ee485f
imath <math.viet@gmail.com>
2022-11-14 06:45:58 +0100

    Move `bp_attachments_set_media_blocks_attached_activity()` into the bp-attachments-activity.php file

--- bp-attachments/bp-attachments-activity.php
+++ bp-attachments/bp-attachments-activity.php
@@ -192,3 +192,51 @@ function bp_attachments_activity_loader() {
 	add_action( 'bp_after_activity_post_form', 'bp_attachments_activity_after_post_form' );
 }
 add_action( 'bp_screens', 'bp_attachments_activity_loader' );
+
+/**
+ * Looks inside a saved activity content to eventually attached its ID to found media.
+ *
+ * @since 1.0.0
+ *
+ * @param BP_Activity_Activity $activity The saved activity object.
+ */
+function bp_attachments_set_media_blocks_attached_activity( $activity = null ) {
+	if ( ! isset( $activity->id ) || ! isset( $activity->content ) ) {
+		return;
+	}
+
+	if ( has_blocks( $activity->content ) ) {
+		$blocks = parse_blocks( $activity->content );
+
+		foreach ( $blocks as $block ) {
+			// @todo replace with a function to get all BP Attachments block names.
+			if ( in_array( $block['blockName'], array( 'bp/image-attachment' ), true ) && isset( $block['attrs']['url'] ) ) {
+				$medium_id   = wp_basename( trim( $block['attrs']['url'], '/' ) );
+				$medium_path = bp_attachments_get_medium_path( $block['attrs']['url'] );
+				$medium_json = trailingslashit( $medium_path ) . $medium_id . '.json';
+
+				// Get data about the medium.
+				$medium_data = wp_json_file_decode( $medium_json );
+				$attributes  = array(
+					'object_type' => 'activity',
+					'object_id'   => $activity->id,
+				);
+
+				$is_existing = false;
+				if ( isset( $medium_data->attached_to ) ) {
+					$is_existing = ! empty( wp_list_filter( $medium_data->attached_to, $attributes ) );
+				} else {
+					$medium_data->attached_to = array();
+				}
+
+				if ( ! $is_existing ) {
+					$medium_data->attached_to[] = (object) $attributes;
+					$media                      = bp_attachments_sanitize_media( $medium_data );
+
+					file_put_contents( $medium_json, wp_json_encode( $media ) ); // phpcs:ignore
+				}
+			}
+		}
+	}
+}
+add_action( 'bp_activity_after_save', 'bp_attachments_set_media_blocks_attached_activity', 10, 1 );
