54d1224e5de3027dd0e83fdb0bac09f5e3e0db84
imath <math.viet@gmail.com>
2022-12-18 15:16:51 +0100

    Add a BP Attachments directory nav

--- templates/attachments/index.php
+++ templates/attachments/index.php
@@ -11,7 +11,10 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+bp_attachments_tracking_output_directory_nav();
 ?>
+
 <div id="bp-media-directory" class="bp-attachments-media-list"></div>
 
 <?php bp_attachments_get_javascript_template( 'media-directory' );
