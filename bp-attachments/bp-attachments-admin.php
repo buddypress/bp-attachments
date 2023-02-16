47e993a8d3b5314c95d5da02cc07d1d574d304a8
imath <math.viet@gmail.com>
2023-02-17 00:19:20 +0100

    Add a link to Attachments options inside Plugin screen table row

--- bp-attachments/bp-attachments-admin.php
+++ bp-attachments/bp-attachments-admin.php
@@ -97,3 +97,41 @@ function bp_attachments_admin_load_settings() {
 	wp_enqueue_script( 'bp-attachments-admin' );
 }
 add_action( 'load-settings_page_bp-settings', 'bp_attachments_admin_load_settings' );
+
+/**
+ * Adds a link to BP Attachments options when needed.
+ *
+ * @since 1.0.0
+ *
+ * @param array  $links Links array in which we would prepend our link.
+ * @param string $file  Current plugin basename.
+ * @return array Processed links.
+ */
+function bp_attachments_admin_plugin_action_links( $links = array(), $file = '' ) {
+	// Return normal links if not BP Attachments.
+	if ( 'bp-attachments/class-bp-attachments.php' !== $file ) {
+		return $links;
+	}
+
+	$settings_page = 'options-general.php';
+	if ( bp_core_do_network_admin() ) {
+		$settings_page = 'settings.php';
+	}
+
+	$options_link = add_query_arg(
+		array(
+			'page' => 'bp-settings',
+		),
+		bp_get_admin_url( $settings_page )
+	);
+
+	// Add a links to Attachments options.
+	return array_merge(
+		$links,
+		array(
+			'settings' => '<a href="' . esc_url( $options_link ) . '#bp-attachments">' . esc_html__( 'Settings', 'bp-attachments' ) . '</a>',
+		)
+	);
+}
+add_filter( 'plugin_action_links', 'bp_attachments_admin_plugin_action_links', 10, 2 );
+add_filter( 'network_admin_plugin_action_links', 'bp_attachments_admin_plugin_action_links', 10, 2 );
